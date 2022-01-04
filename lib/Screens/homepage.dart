7355bfdb088a5fec70b4ffb2c16d2d3317cb4cff
Rahul Mardiya <rahul.theappideas@gmail.com>
2022-01-04 11:28:56 +0530

    day11

--- lib/Screens/homepage.dart
+++ lib/Screens/homepage.dart
@@ -7,11 +7,11 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int days = 4;
-    String name = " Today I'm joining TheAppIdeas Company";
-    double pi = 3.14;
-    num temp = 50.5;
-    var data = "hello how are you";
+    // int days = 4;
+    // String name = " Today I'm joining TheAppIdeas Company";
+    // double pi = 3.14;
+    // num temp = 50.5;
+    // var data = "hello how are you";
     studentsMethod();
     return Scaffold(
       appBar: AppBar(
@@ -20,15 +20,16 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Center(
-            child: Text("i completed $days Months internship in flutter"),
+          Container(
+            constraints: const BoxConstraints(
+                maxHeight: 200, maxWidth: 210, minHeight: 10, minWidth: 20),
+            child: Container(
+              height: 500,
+              width: 500,
+              color: Colors.blueGrey,
+            ),
           ),
-          Text(name),
-          Text("$pi"),
-          Text("$temp"),
-          Text(data)
         ],
       ),
       drawer: const MyDrawer(),
