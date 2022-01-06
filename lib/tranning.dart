e6ca340878a94a679be663dc6e52103e1b9ef1fd
Rahul Mardiya <rahul.theappideas@gmail.com>
2022-01-06 19:34:23 +0530

    student api add

--- lib/tranning.dart
+++ lib/tranning.dart
@@ -1,5 +1,6 @@
 import 'package:day1/App1/Screens/login.dart';
 import 'package:day1/App2/pages/homepage.dart';
+import 'package:day1/Students/home_students.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -17,12 +18,17 @@ class Tranning extends StatelessWidget {
           ElevatedButton(
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => const LoginScreen())),
-              child: "App1".text.make()),
+              child: "Shopping".text.make()),
           const Divider(),
           ElevatedButton(
               onPressed: () => Navigator.push(context,
                   MaterialPageRoute(builder: (context) => const HomePage())),
-              child: "App2".text.make()),
+              child: "Api Integration".text.make()),
+          const Divider(),
+          ElevatedButton(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const HomeStudents())),
+              child: "Api Students".text.make()),
         ],
       ).p32(),
     );
